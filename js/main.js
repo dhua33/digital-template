6f02624a549a83e0818ad3cdda906df001408a49
dhua33 <dhua3@gmu.edu>
2015-02-02 20:56:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ window.onload = function() {
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
         game.load.spritesheet( 'ninja', 'assets/ninja.png', 500, 500);
     }
     
