3e4c2ba16456c2bff0f3a8f2d17d8b631582a711
dhua33 <dhua3@gmu.edu>
2015-02-04 15:15:41 -0500

    Large update
    add better animations, background, platforms, collisions, death restart
    and win state

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ window.onload = function() {
     
     "use strict";
     
-    var game = new Phaser.Game( 800, 400, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var game = new Phaser.Game( 1000, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
         // load sprites and backgrounds
         game.load.image('sky', 'assets/sky.bmp');
-        game.load.spritesheet( 'ninja', 'assets/ninja.png', 536, 522);
-        game.load.image('map', 'assets/map.bmp');
+        game.load.spritesheet( 'ninja', 'assets/ninja.png', 489, 489);
         game.load.image('dog', 'assets/dog.png');
+        game.load.image('menu', 'assets/menu.png');
+        game.load.image('back', 'assets/background.png');
+        game.load.image('spike', 'assets/spike.png');
+        game.load.image('ball', 'assets/ball.png');
+        game.load.image('platform', 'assets/platform.png');
     }
     // create variables
     var bg;
@@ -19,20 +23,87 @@ window.onload = function() {
     var jumpTimer = 0;
     var facingLeft = false;
     var dog;
+    var menu;
+    var back;
+    var platforms;
+    var plat;
+    var spikes;
+    var spike;
+    var balls;
+    var ball;
+    var collidePlat;
+    var collideBall;
+    var collideSpike;
+    var text;
+    var style;
+    var style2;
+    var i = 2000;
+    var win;
     
     function create() {
         // add sprites and turn on the arcade physics engine for this sprite.
-        bg = game.add.tileSprite(0, 0, 30000, 400, 'sky');
-        player = game.add.sprite(15, 15, 'ninja');
+        bg = game.add.tileSprite(0, 0, 30000, 1600, 'sky');
+        back = game.add.tileSprite(0, 0, 30000, 1600, 'back');
+        player = game.add.sprite(15, 600, 'ninja');
         player.scale.setTo(0.2, 0.2);
-        game.physics.enable(player, Phaser.Physics.ARCADE );
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.enable(player);
         game.world.sendToBack(bg);
-        dog = game.add.sprite(29800, 330, 'dog');
+        dog = game.add.sprite(29800, 1330, 'dog');
         dog.scale.setTo(0.4, 0.4);
+        game.physics.arcade.enable(dog);
+        
+        // add platforms and obstacles
+        platforms = game.add.group();
+        spikes = game.add.group();
+        balls = game.add.group();
+        platforms.enableBody = true;
+        spikes.enableBody = true;
+        balls.enableBody = true;
+        // dog's platform
+        plat = platforms.create(29700, 1370, 'platform');
+        plat.scale.setTo(2, 1);
+        plat.body.immovable = true;
+        
+        // base platforms
+        while (i < 29000) {
+        		plat = platforms.create(i, 1200, 'platform');
+        		plat.scale.setTo(10, .5);
+        		plat.body.immovable = true;
+        		i += 3000;	
+        } i = 0;
+        while (i < 30000) {
+        		plat = platforms.create(i, 1400, 'platform');
+        		plat.scale.setTo(16, .5);
+        		plat.body.immovable = true;
+        		i += 4500
+        } i = 3900;
+        while (i < 27600) {
+        		plat = platforms.create(i, 1000, 'platform');
+        		plat.scale.setTo(16, .5);
+        		plat.body.immovable = true;
+        		i += 3100
+        } i = 3250;
+        // staircase up
+        var j = 0;
+        while (i > 0) {
+        		plat = platforms.create(i, 800 - j, 'platform');
+        		plat.scale.setTo(2, .7);
+        		plat.body.immovable = true;
+        		i -= 600;
+        		j += 50;
+        } i = 700;
+        // large plats
+        while (i < 28950) {
+        		plat = platforms.create(i, 350, 'platform');
+        		plat.scale.setTo(32, .5);
+        		plat.body.immovable = true;
+        		i += 5650
+        }
+        
         // add animations
-        var j = 10;
-        var arrayJump = [], arrayRun = [], arraySlide = [];
-        var arrayIdle = [], arrayDie = [];
+        j = 10;
+        var arrayJump = [], arrayRun = [], arraySlide = [], arrayIdle = [], arrayDie = [];
         while(j < 40) {
         		if(j < 20)
         				arrayDie.push(j);
@@ -51,22 +122,59 @@ window.onload = function() {
         		j++;
         }  
         player.animations.add('idle', arrayIdle, 20, true);
-        player.animations.add('jump', arrayJump, 20, true);
+        player.animations.add('jump', arrayJump, 15, true);
         player.animations.add('run', arrayRun, 20, true);
         player.animations.add('slide', arraySlide, 20, true);
-        // create world gravity and camera
-        game.physics.arcade.gravity.y = 3200;
+        player.animations.add('die', arrayDie);
+        player.anchor.setTo(.5, .5);
+        // create player gravity and camera
+        player.body.gravity.y = 3200;
         game.camera.follow(player);
         // set world bounds and key inputs
+        game.world.setBounds(0, 0, 30000, 1600);
         player.body.collideWorldBounds = true;
         keys = game.input.keyboard.createCursorKeys();
         jump = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
-    		player.anchor.setTo(.5, .5);
+        // death state text
+    		style = { font: "25px Verdana", fill: "#ff0000", align: "center" };
+    		style2 = { font: "25px Verdana", fill: "#ffff00", align: "center" };
+    		text = game.add.text(200, 200, "", style);
+    		text.fixedToCamera = true;
+        
+        // pause the game in the beginning to display controls
+        jump.onDown.add(exitMenu, this);
+        menu = game.add.sprite(100, 0, 'menu');
+        game.paused = true;
+        i = 0;
+    }
+    function exitMenu() {
+    		if(!win) {
+    				menu.destroy();
+    				game.paused = false;
+    		}
     }
     
     function update() {
-    if (!player.body.onFloor()) { // if the player is not on the ground
+    // collision
+    		collidePlat = game.physics.arcade.collide(player, platforms);
+    		win = game.physics.arcade.collide(player, dog);
+    // win
+    if(win){
+    		text.setStyle(style2);
+    		text.setText("YOU WIN\nYou've caught Memory.");
+    		game.paused = true;
+    }
+    // death state
+    if(player.body.onFloor()) {
+        player.body.velocity.x = 0;
+        back.stopScroll();
+        bg.stopScroll();
+        text.setText("YOU DIED\nPress the up arrow key to restart.");
+    		player.animations.play('die', 20, false, true);
+    }
+    // controls
+    else if (!player.body.onFloor() && !collidePlat) { // if the player is not on the ground
     		player.animations.play('jump');
     }
     else if (keys.left.isDown)
@@ -75,9 +183,11 @@ window.onload = function() {
     				player.scale.x *= -1; // flip the sprite
     				facingLeft = true;
     		}
-    		if (player.body.velocity.x > -1000)
+    		if (player.body.velocity.x > -700)
     				player.body.velocity.x += -20;
         player.animations.play('run');
+        back.autoScroll(5, 0);
+        bg.autoScroll(150, 0);
     }
     else if (keys.right.isDown)
     {
@@ -85,9 +195,11 @@ window.onload = function() {
     				player.scale.x *= -1;
     				facingLeft = false;
     		}
-    		if (player.body.velocity.x < 1000)
+    		if (player.body.velocity.x < 700)
     				player.body.velocity.x += 20;
         player.animations.play('run');
+        back.autoScroll(-5, 0); 
+        bg.autoScroll(-150, 0);
     }
     else if (keys.down.isDown && player.body.velocity.x != 0) { // sliding physics
     		if(player.body.velocity.x > 0)
@@ -99,13 +211,20 @@ window.onload = function() {
     else
     {
         player.body.velocity.x = 0;
+        back.stopScroll();
+        bg.stopScroll();
         player.animations.play('idle');
     }
-    
-    if (jump.isDown && player.body.onFloor() && game.time.now > jumpTimer)
+    // jumping
+    if (jump.isDown && collidePlat && game.time.now > jumpTimer)
     {
-        player.body.velocity.y = -1000;
+        player.body.velocity.y = -1200;
         jumpTimer = game.time.now + 500;
+    }
+    // reset
+    if(!player.alive && keys.up.isDown) {
+    		player.reset(15, 700);
+    		text.setText("");
     }
     }
 };
